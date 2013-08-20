b9f1304721f33897f85262a8b4fcc0ab13830ca9
Matt Richards <marichards88@gmail.com>
2013-08-20 15:52:04 -0700

    Basic Views and URLs that display strings

---
+++ media_site/defined_media/urls.py
@@ -0,0 +1,35 @@
+#URLs file for defined media database
+from django.conf.urls import patterns, url
+#Include the views I've written
+from defined_media import views
+
+#URL Format:
+#url(regex,view,*name,*kwargs)
+#Regex defines the pattern for the url
+#View defines which view to call for that page
+#Kwargs is a keyword argument...not sure what to do with this
+#Name lets you refer to it somewhere else, like a template 
+#-->Powerful, lets you make global changes to url patterns
+
+#Define ALL the urls!
+urlpatterns = patterns('',
+	#Simple Response url (Main Page)
+	url(r'^$', views.main, name='main'),
+	
+	#Main Indices
+	url(r'^compounds/$', views.compounds, name='compounds'),
+	url(r'^organisms/$', views.organisms, name='organisms'),
+	url(r'^media/$', views.media, name='media'),
+	url(r'^biomass/$', views.biomass, name='biomass'),
+	url(r'^sources/$', views.sources, name='sources'),
+
+	#Record-Specific Views
+	url(r'^compounds/(?P<compid>\d+)/$', views.compound_record, name='compound_record'),
+	url(r'^organisms/(?P<strainid>\d+)/$', views.organism_record, name='organism_record'),
+	url(r'^media/(?P<medid>\d+)/$', views.media_record, name='media_record'),
+	url(r'^biomass/(?P<biomassid>\d+)/$', views.biomass_record, name='biomass_record'),
+	url(r'^sources/(?P<sourceid>\d+/$', views.source_record, name='source_record'),
+)
+
+#Define the Admin URL too, but in the main media_site directory
+
--- media_site/defined_media/views.py
+++ media_site/defined_media/views.py
@@ -1,1 +1,47 @@
 # Create your views here.
+
+from django.http import HttpResponse
+
+#Define the main page of the site
+def main(request):
+	return HttpResponse('This will be the main page someday!')
+
+#Define the compounds index
+def compounds(request):
+	return HttpResponse('This is the main compounds index')
+
+#Define the media index
+def media(request):
+	return HttpResponse('This is the main media index')
+
+#Define the organisms index
+def organisms(request):
+	return HttpResponse('This is the main organisms index')
+
+#Define the biomass index
+def biomass(request):
+	return HttpResponse('This is the main biomass index')
+
+#Define the sources index
+def sources(request):
+	return HttpResponse('This is the main sources index')
+
+#Define Record-Specific Compound View
+def compound_record(request,compid):
+	return HttpResponse('This is the page for compound %s.' %compid)
+
+#Define Record-Specific Organisms View
+def organism_record(request, strainid):
+	return HttpResponse('This is the page for organism %s' %strainid)
+
+#Define Record-Specific Media View
+def media_record(request, medid):
+	return HttpResponse('This is the page for media %s' %medid)
+
+#Define Record-Specific Biomass View
+def biomass_record(request, biomassid):
+	return HttpResponse('This is the page for biomass %s' %biomassid)
+
+#Define Record-Specific Source View
+def source_record(request, sourceid):
+	return HttpResponse('This is the page for source %s' %sourceid)
