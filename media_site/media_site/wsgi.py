473f7f9a2b89e5ec400a1fc03128aab2847c7a6c
Victor Cassen <phonybone@gmail.com>
2013-10-04 15:27:36 -0700

    tweaking for production release

--- media_site/media_site/wsgi.py
+++ media_site/media_site/wsgi.py
@@ -13,12 +13,14 @@ middleware here, or combine a Django application with an application of another
 framework.
 
 """
-import os
+import os, sys
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "media_site.settings"
+sys.path.append('/home/victor/l/media-db/media_site')
+sys.path.append('/local/vpython/lib/python2.7/site-packages')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "media_site.settings")
 
 # This application object is used by any WSGI server configured to use this
