6bd9310b47e4c211eaa25a2257ca6e6af096a860
Victor Cassen <phonybone@gmail.com>
2014-01-13 16:27:18 -0800

    added versatility to compounds_fields.py

--- media_site/defined_media/scripts/compound_fields.py
+++ media_site/defined_media/scripts/compound_fields.py
@@ -5,85 +5,160 @@ init_django_env(root_dir, 'media_site', 'defined_media')
 import django
 from defined_media.models import *
 
-parser=argparse.ArgumentParser()
-parser.add_argument('in_fn')
-parser.add_argument('field1')
-parser.add_argument('field2')
-parser.add_argument('-v', action='store_true')
-parser.add_argument('-n', action='store_true')
-parser.add_argument('-flip', action='store_true')
-opts=parser.parse_args()
-if opts.v: print 'opts: %s' % opts
+'''
+Add data columns to the compounds table:
 
-def flip(v1,v2):
-    tmp=v1
-    v1=v2
-    v2=tmp
-    return (v1,v2)
+Parse a csv (or tsv); for each line,
+extract a lookup value
+extract a data value (to add to table), add to list
+for both values, verify integrity via a regex
 
-f2re={'seed_id': r'^cpd\d+$',
-      'kegg_id': r'^C\d+$',
-      'chebi_id': r'^\d+$',
-      'pubchem_id': r'^\d+$'}
+for lookup, values in parsed_data.items():
+  lookup Compound, modify according to values, and save
+  
 
-f1=opts.field1
-f2=opts.field2
-if opts.flip:
-    (f1,f2)=flip(f1,f2)
-f12f2={}
-stats={1:0, 2:0, 3:0, 'n_comps':0, 'n_missing':0}
-stats['bad_'+f1]=0
-stats['bad_'+f2]=0
+Inputs:
+- filename
+- lookup fieldname, column number
+- novel fieldname, column numer
+- regexes for each type of field value (currently hardcoded)
+'''
 
-with open(opts.in_fn) as f:
-    reader=csv.reader(f, delimiter='\t')
-    for row in reader:
-        v1,v2=row
-        if opts.flip:
-            (v1,v2)=flip(v1,v2)
 
-        if f1 in f2re:
-            if not re.search(f2re[f1], v1):
-                stats['bad_'+f1]+=1
-                if opts.v:
-                    print 'bad %s: %s' % (f1, v1)
+def get_fs():
+    ''' return the list of field names as obtained from the first line of the file '''
+    with open(opts.in_fn) as f:
+        line1=f.readline()
+        print 'line1 is %s' % line1
+        fs=[x.strip() for x in re.split(r'\s+', line1)]
+        print 'fs is %s' % fs
+        if fs[0].startswith('#'):
+            del fs[0]
+            print 'fs is %s' % fs
+        return fs
+
+def verify(f,v):
+    reg=re.compile(f2re[f])
+    if re.search(reg, v):
+        return True
+    else:
+        stats['bad_'+f]+=1
+        if opts.v:
+            print 'bad %s: "%s"' % (f, v)
+        return False
+
+
+def parse_file(lu_idx, val_idx):
+    '''
+    build f12f2 s.t. k=lookup key, v=associated value
+    '''
+    f12f2={}
+    with open(opts.in_fn) as f:
+        reader=csv.reader(f, delimiter='\t')
+        for row in reader:
+            if row[0].startswith('#'):
                 continue
-        if f2 in f2re:
-            if not re.search(f2re[f2], v2):
-                stats['bad_'+f2]+=1
-                if opts.v:
-                    print 'bad %s: %s' % (f2, v2)
-                continue
 
+            lu_val=row[lu_idx].strip()
+            val=row[val_idx].strip()
+            if not verify(opts.lookup, lu_val): continue
+            if not verify(opts.novel, val): continue
+
+            try:
+                f12f2[lu_val].append(val)
+            except KeyError:
+                f12f2[lu_val]=[val]
+            if opts.v:
+                print '%s: %s' % (lu_val, f12f2[lu_val])
+    return f12f2
+
+def update_compounds(f12f2):
+    for lu,nv in f12f2.items():
+        # lookup compound:
+        args={opts.lookup: lu}
         try:
-            f12f2[v1].append(v2)
+            comp=Compounds.objects.get(**args)
+        except Compounds.DoesNotExist:
+            if opts.v: print 'no compound for %s=%s' % (opts.lookup, lu)
+            stats['n_missing']+=1
+            continue
+
+        # store comma-separated list of values to compound:
+        nvs=','.join(nv)
+        setattr(comp, opts.lookup, nv)
+        if opts.v:
+            print 'about to save %s (%s=%s): set %s to %s' % (comp, opts.lookup, lu, opts.novel, nvs)
+        if not opts.n: 
+            comp.save()
+        stats['n_comps']+=1
+
+        # compile stats "histogram":
+        n=len(nv)
+        try:
+            stats[n]+=1
         except KeyError:
-            f12f2[v1]=[v2]
+            stats[n]=1
 
-for v1,v2l in f12f2.items():
-    args={f1:v1}
+def report(stats):
+    for n in sorted(stats.keys()):
+        print '%s: %s' % (n, stats[n])
+
+
+def get_args():
+    parser=argparse.ArgumentParser()
+    parser.add_argument('in_fn')
+    parser.add_argument('lookup')
+    parser.add_argument('novel')
+    parser.add_argument('-v', action='store_true')
+    parser.add_argument('-n', action='store_true')
+
+    opts=parser.parse_args()
+    opts.lookup=opts.lookup.lower()
+    opts.novel=opts.novel.lower()
+    return opts, parser
+
+
+if __name__=='__main__':
+    opts,parser=get_args()
+    if opts.v: print 'opts: %s' % opts
+    
+    f2re={
+        'compid': r'^\d+$',
+        'seed_id': r'^cpd\d+$',
+        'kegg_id': r'^C\d+$',
+        'chebi_id': r'^\d+$',
+        'pubchem_ids': r'^\d+$',
+        'formula': r'^[()\w]+$',
+        }
+
     try:
-        comp=Compounds.objects.get(**args)
-    except Compounds.DoesNotExist:
-        if opts.v: print 'no compound for %s=%s' % (f1, v1)
-        stats['n_missing']+=1
-        continue
+        fields=get_fs()         # can throw
+        for i,f in enumerate([f.lower() for f in fields]):
+            if f==opts.lookup:
+                lookup_f=f.lower()
+                lookup_idx=i
+            elif f==opts.novel:
+                novel_f=f.lower()
+                novel_idx=i
+    except RuntimeError as e:
+        print str(e)
+        parser.print_help()
+        sys.exit(1)
 
-    v2s=','.join(v2l)
-    setattr(comp, f2, ','.join(v2l))
-    if opts.v:
-        print 'about to save %s (%s=%s): set %s to %s' % (comp, f1, v1, f2, v2s)
-    if not opts.n: 
-        comp.save()
-    stats['n_comps']+=1
 
-    n=len(v2l)
-    if n>4:
-        print 'big_n=%d, v1=%s, v2l=%s' % (n, v1, v2l)
+    # verify existence of lookup_f and novel_f:
     try:
-        stats[n]+=1
-    except KeyError:
-        stats[n]=1
+        s='lookup: %s\tnovel: %s' % (lookup_f, novel_f)
+    except NameError:
+        print 'missing field args (lookup or novel)'
+        parser.print_help()
+        sys.exit(1)
+            
 
-for n in sorted(stats.keys()):
-    print '%s: %s' % (n, stats[n])
+    stats={1:0, 2:0, 3:0, 'n_comps':0, 'n_missing':0}
+    stats['bad_'+lookup_f]=0
+    stats['bad_'+novel_f]=0
+
+    f12f2=parse_file(lookup_idx, novel_idx)
+    update_compounds(f12f2)
+    report(stats)
